27c8675ecdce6952f566ebdc3df0019871e3fdc9
Fatima Goncalves <124080552+cx-fatima-goncalves@users.noreply.github.com>
2026-08-17 17:32:01 +0100

    Checkmarx Bot - Auto Pull Request

--- SQLi_1 - Copy/Program.cs
+++ SQLi_1 - Copy/Program.cs
@@ -37,21 +37,25 @@ namespace SQLi_1
             {
                 using (var conn = new SqlConnection("conn..."))
                 {
-                    var sql = "SELECT * FROM Users WHERE username = '" + username + "' AND pwd = '" + password + "'";
-                    using (var cmd = new SqlCommand(sql))
+                    // Use a parameterized query to prevent SQL injection.
+                    // User-supplied values are passed as SqlParameter objects,
+                    // never interpolated into the SQL string.
+                    var sql = "SELECT * FROM Users WHERE username = @username AND pwd = @password";
+                    using (var cmd = new SqlCommand(sql, conn))
                     {
-                        cmd.Connection = conn;
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password);
                         cmd.ExecuteScalar();
                     }
 
                 }
             }
-            catch  
+            catch
             {
 
                 Console.WriteLine("An error has occurred !!");
             }
-           
+
         }
     }
 }
